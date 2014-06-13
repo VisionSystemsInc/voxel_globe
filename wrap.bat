b29fc33566b6f05acb7fb3562a9bf4b929157292
Andrew Neff <andrew.neff@visionsystemsinc.com>
2014-06-13 19:15:48 -0400

    Updated log, rabbitmq, etc...

--- wrap.bat
+++ wrap.bat
@@ -11,8 +11,8 @@ if "%1"=="start_httpd" (
   cd %PROCESSOR_DIR%
   celery worker -A tasks --logfile=%LOG_DIR%/celery_log.txt --loglevel=INFO > %LOG_DIR%/celery_out.txt 2> %LOG_DIR%/celery_err.txt
 ) else if "%1"=="start_rabbitmq" (
-  set HOMEDRIVE=%CONF_DRIVE%
-  set HOMEPATH=%CONF_PATH%
+  set HOMEDRIVE=%RABBITMQ_BASE_DRIVE%
+  set HOMEPATH=%RABBITMQ_BASE_PATH%
   rabbitmq-server  > %LOG_DIR%/rabbitmq_out.txt 2> %LOG_DIR%/rabbitmq_err.txt
 ) else if "%1"=="start_postgresql" (
   postgres -D %DATABASE_DIR% -p %DATABASE_PORT% > %LOG_DIR%/postgresql_out.txt 2> %LOG_DIR%/postgresql_err.txt
