ea6b81d58772ada905b762510b9449a5b8a8f2ad
Andy Neff <andrew.neff@visionsystemsinc.com>
2014-07-16 17:38:41 -0400

    Got celery and django talking with each other

--- processors/tasks.py
+++ processors/tasks.py
@@ -3,6 +3,8 @@ from celery import Celery
 import scipy;
 import numpy;
 
+import world.models
+
 app = Celery('tasks');
 app.config_from_object('celeryconfig') #Don't need this because celeryd does it for me
 
@@ -15,3 +17,7 @@ def add(x, y):
     aaa = scipy.ifft(aa);
   return numpy.abs(a-aaa).sum();
 
+@app.task
+def getArea(id):
+  country = world.models.WorldBorder.objects.get(id=id)
+  return country.area;
