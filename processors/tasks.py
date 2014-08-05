12e7bb5d2e47ba296891fb3edcfe3ec0545e5365
Andy Neff <andrew.neff@visionsystemsinc.com>
2014-08-05 15:35:11 -0400

    Tests in task before cleanup

--- processors/tasks.py
+++ processors/tasks.py
@@ -1,23 +1,175 @@
-from celery import Celery
+from celery import Celery, Task, group
 
 import scipy;
 import numpy;
 
 import world.models
+import meta.models
+
+from os import environ as env;
 
 app = Celery('tasks');
 app.config_from_object('celeryconfig') #Don't need this because celeryd does it for me
 
-@app.task
-def add(x, y):
+import json;
+
+@app.task(bind=True)
+def add(self, x, y):
   a = numpy.random.random((x,x));
   aaa = a;
   for yy in xrange(y):
     aa = scipy.fft(aaa);
     aaa = scipy.ifft(aa);
-  return numpy.abs(a-aaa).sum();
+
+  return [numpy.abs(a-aaa).sum(), self.request.id]
 
 @app.task
 def getArea(id):
   country = world.models.WorldBorder.objects.get(id=id)
   return country.area;
+
+def serviceWrap(service):
+  def inner(*args, **kwargs): 
+    preTask(args, kwargs);
+    outputs = service(*args, **kwargs);
+    postTask(outputs);
+    return outputs;
+  return inner;
+
+def preTask(args, kwargs):
+  ##TODO: Add Parameters instance
+  print 'args are', args
+  print 'kwargs are', kwargs
+  pass;
+
+def postTask(outputs):
+  ##TODO Tie results to Parameters, update output,
+  print "All done!"
+
+class VipTask(Task):
+  abstract = True
+  
+  def __getTaskId(self, inputs=None):
+    serviceInstance = meta.models.ServiceInstance(
+                          inputs=json.dumps(inputs),
+                          outputs='NAY', step=0, name='NAY');
+    serviceInstance.save();
+    return serviceInstance.id;
+
+  def apply_async(self, args=None, kwargs=None, task_id=None, *args2, **kwargs2):
+    if not task_id:
+      task_id = self.__getTaskId((args, kwargs));
+    return super(VipTask, self).apply_async(args=args, kwargs=kwargs, task_id=task_id, *args2, **kwargs2)
+  
+  def apply(self, args=None, kwargs=None, task_id=None, *args2, **kwargs2):
+    ''' Automatically create task_id's based off of new primary keys in the database''' 
+    if not task_id:
+      task_id = self.__getTaskId((args, kwargs));
+    return super(VipTask, self).apply(args=args, kwargs=kwargs, task_id=task_id, *args2, **kwargs2)
+    
+  
+#  def after_return(self, status, retval, task_id, args, kwargs, einfo): #, *args2, **kwargs2
+  def on_success(self, retval, task_id, args, kwargs):
+    #I can't currently tell if apply or apply_asyn is called, but I don't think I care
+    try:
+      serviceInstance = meta.models.ServiceInstance.objects.get(id=task_id);
+    except (meta.models.ServiceInstance.DoesNotExist, ValueError):
+      serviceInstance = meta.models.ServiceInstance(
+            step=0,
+            inputs=json.dumps((args, kwargs)));
+    
+    serviceInstance.outputs = json.dumps(retval)
+    serviceInstance.name = task_id;
+    serviceInstance.save();
+  
+  def on_failure(self, exc, task_id, args, kwargs, einfo):
+    self.on_success(str(einfo), task_id, args, kwargs);
+
+@app.task(base=VipTask)
+def test(abc=None, *args, **kwargs):
+  if abc==15:
+    raise Exception("ouch");
+  return 42
+
+@app.task
+@serviceWrap
+def add_sample_data():
+  print 'Adding Sample data'
+  img = meta.models.Image(name="Oxford Codrington Library", imageWidth=999, imageHeight=749, 
+                          numberColorBands=3, pixelFormat='b', fileFormat='zoom', 
+                          imageURL='http://%s:%d/static/meta/images/camelot-UK_2012OxfordUniversity-42' % 
+                                   (env['NPR_POSTGRESQL_HOST'], int(env['NPR_HTTPD_PORT'])));
+                                  #I know postgresql here is WRONG, don't care right now, it WILL be image server!
+  img.save();
+
+  tp = meta.models.ImageTiePoint(x=100, y=100, name='Some point', image = img);
+  tp.save();
+  
+  gtp = meta.models.GeoTiePoint(name='Some geo point', 
+           description='None provided. Just some point trying to make a point in life',
+           latitude=51.7534, longitude=-1.2539, altitude=89.2,
+           apparentLatitude=51.753416, apparentLongitude=-1.254033, apparentAltitude=71)
+  gtp.save();
+  
+  tp = meta.models.ImageTiePoint.objects.get(name='Some point');
+  #This previous command is NOT neccesary, since tp is still in memory, 
+  #HOWEVER I wanted to provice an update example
+  tp.geoPoint = gtp;
+  tp.save();
+
+#TODO
+#Define Add task
+#--Addes a task, retrieve data from database, etc...
+#--Gets a GUID and sets task ID
+#--Adds entry for service/taskID into ____ table
+
+#Workflows?
+#Need to read up on Celery to see if it already does workflows
+
+'''
+Avoid launching synchronous subtasks
+
+Having a task wait for the result of another task is really inefficient, and may even cause a deadlock if the worker pool is exhausted.
+
+Make your design asynchronous instead, for example by using callbacks.
+
+Bad:
+
+@app.task
+def update_page_info(url):
+    page = fetch_page.delay(url).get()
+    info = parse_page.delay(url, page).get()
+    store_page_info.delay(url, info)
+
+@app.task
+def fetch_page(url):
+    return myhttplib.get(url)
+
+@app.task
+def parse_page(url, page):
+    return myparser.parse_document(page)
+
+@app.task
+def store_page_info(url, info):
+    return PageInfo.objects.create(url, info)
+
+Good:
+
+def update_page_info(url):
+    # fetch_page -> parse_page -> store_page
+    chain = fetch_page.s() | parse_page.s() | store_page_info.s(url)
+    chain()
+
+@app.task()
+def fetch_page(url):
+    return myhttplib.get(url)
+
+@app.task()
+def parse_page(page):
+    return myparser.parse_document(page)
+
+@app.task(ignore_result=True)
+def store_page_info(info, url):
+    PageInfo.objects.create(url=url, info=info)
+
+Here I instead created a chain of tasks by linking together different subtask()'s. You can read about chains and other powerful constructs at Canvas: Designing Workflows.'''
