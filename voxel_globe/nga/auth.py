02277263948750be3710a7c65362cddebb69c53c
Andrew Neff <andrew.neff@visionsystemsinc.com>
2014-12-09 11:43:58 -0500

    Removed old lines

--- voxel_globe/nga/auth.py
+++ voxel_globe/nga/auth.py
@@ -15,37 +15,6 @@ from django.contrib.sessions.models import Session
 from django import setup;
 setup();
 
-'''def check_password(environ, user, password):
-    return True
-    db.reset_queries() 
-    
-    check_password.a+=1
-    print environ['HTTP_COOKIE'], check_password.a
-    
-    kwargs = {'username': user, 'is_active': True} 
-
-    try: 
-        try: 
-            user = User.objects.get(**kwargs) 
-        except User.DoesNotExist: 
-            return None
-
-        if user.check_password(password): 
-            return True
-        else: 
-            return False
-    finally: 
-        db.connection.close()
-check_password.a = 1'''
-
-'''class SessionObject(object):
-  def __init__(self, sessionId, expireTime):
-    self.sessionId = sessionId;
-    self.expireTime = expireTime;
-    
-  def __eq__(self, rhs):
-    return self.sessionId == rhs.sessionId;'''
-
 def allow_access(environ, host):
   #print '\n'.join(map(lambda x: '%s:  %s' % x, zip(environ.keys(), environ.values())))
   #print environ['REQUEST_URI'], environ['SCRIPT_NAME']
@@ -115,51 +84,6 @@ def allow_access(environ, host):
     #print 'Invalid time'
     return False
   
-
-
-  '''try:
-    session = SessionObject(cookie['sessionid'].value, datetime.datetime.now(tz=pytz.utc));
-    print 'Session found', session.sessionId
-    
-    #Handle clearing cache
-    if session.expireTime > allow_access.nextCheck:
-      #if checkFrequency has passed, clear the list
-      allow_access.validSessions = []
-      print 'Cleared cache'
-    allow_access.nextCheck = session.expireTime + allow_access.checkFrequency;
-
-    try:
-      print 'Check in cache'
-      #Check if in list
-      validIndex = allow_access.validSessions.index(session)
-      #Get index
-      print 'In cache'
-    except ValueError:#Session not in list
-      print 'Not in cache'
-      try:
-        print 'check in session db', session.sessionId
-        sessionDb = Session.objects.get(pk=session.sessionId)
-        #if it's in the database
-        allow_access.validSessions.append(sessionDb)
-        validIndex = len(allow_access.validSessions)-1;
-        #Add to cache
-        print 'in session db'
-      except Session.DoesNotExist:
-        #Not in session DB
-        print 'Not in session db'
-        return False;
-
-    if allow_access.validSessions[validIndex].expireTime > session.expireTime:
-      #Valid session by expire time
-      print 'Valid time'
-      return True
-    else:
-      print 'Invalid time'
-      return False
-    
-  except KeyError:
-    #No session id -> not logged in -> immediate access denied
-    return False'''
 allow_access.validSessions = {}
 allow_access.nextCheck = datetime.datetime.now(tz=pytz.utc);
 allow_access.checkFrequency = datetime.timedelta(seconds=10); 
